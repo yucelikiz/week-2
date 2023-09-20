0bcac28b000ffb278dcdeb61d3292455fc4a3c9c
yucelikiz <97850432+yucelikiz@users.noreply.github.com>
2023-09-20 09:48:11 +0300

    kodlar daha temiz hale getirildi.

--- src/RecursiveUsluSayilar.java
+++ src/RecursiveUsluSayilar.java
@@ -2,24 +2,27 @@ import java.util.Scanner;
 
 public class RecursiveUsluSayilar {
     //sayıların üssünü alan statik bir nesne oluşturuyoruz
-    static int power(int base , int exponent){
+    static int power(){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Taban sayısını giriniz :");
+        int base = scan.nextInt();
+
+        System.out.print("Üs sayısını giriniz :");
+        int exponent = scan.nextInt();
+
+        //sonucu 1e eşitleyerek üssün sıfır aldığı durumlarda otomatik 1 sonucunu veriyoruz, aksi takdirde for döngüsü üs sayısı kadar işlem yapıyor.
         int result = 1;
         for(;exponent != 0 ; exponent--){
             result *= base;
         }
-        System.out.println("Sonuç = " + result);
         return result;
     }
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        //Programın sürekli çalışmasını while döngüsü ile sağlıyoruz.
-        while (true) {
-            System.out.print("Taban sayısını giriniz :");
-            int x = scan.nextInt(); //değişkenleri bu kısımda belirleyip üs alma metoduna ekliyoruz.
-            System.out.print("Üs sayısını giriniz :");
-            int y = scan.nextInt();
-            power(x,y);
+        //Oluşturulan nesne sayesinde tek bir kod satırı ile üs hesabı yapabiliyoruz.
+        while (true)
+        System.out.println("Sonuç = " + power());
         }
-    }
+
 }
