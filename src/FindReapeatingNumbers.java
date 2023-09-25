448204f9878cc580e4d325f01fb2230505915dfe
yucelikiz <97850432+yucelikiz@users.noreply.github.com>
2023-09-26 00:51:23 +0300

    bir dizideki tekrar eden çift sayıları bulan program.

---
+++ src/FindReapeatingNumbers.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class FindReapeatingNumbers {
+    static boolean isFind(int[] arr, int value) {
+        for (int i : arr) {
+            if (i == value ) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int [] list = {3,7,2,3,2,9,10,21,1,33,9,1};
+        int[] dublicate = new int[list.length];
+        int startIndex = 0;
+        for (int i=0; i< list.length; i++) {
+
+            for (int j=0; j < list.length; j++) {
+                if( (i != j) && list[i] == list[j]) {
+                    if (list[i] % 2 == 0) {
+                        if (!isFind(dublicate, list[i])) {
+                            dublicate[startIndex++] = list[i];
+                        }
+                        break;
+                    }
+
+
+
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(dublicate));
+
+    }
+}
