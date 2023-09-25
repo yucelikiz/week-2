5b7324ec731dc82a2f3613f019eb39007b1bc5f9
yucelikiz <97850432+yucelikiz@users.noreply.github.com>
2023-09-25 15:50:58 +0300

    açıklama satırları eklendi.

--- src/HarmonicMeanOfArrays.java
+++ src/HarmonicMeanOfArrays.java
@@ -1,11 +1,18 @@
 public class HarmonicMeanOfArrays {
     public static void main(String[] args) {
+
+        //Dizimizi oluşturuyoruz.
         int[] list = {1,2,3,4,5,6,7};
+
+        //harmonik serimizi tutacak olan değişkeni tanımlıyoruz.
         double harmonic = 0.0;
+
+        //for-each döngüsünü kullanarak listemizdeki her numarayı 1'in böleni olarak hesaplıyoruz ve bunları üst üste topluyoruz.
         for (int number : list) {
             harmonic += 1.0 / number;
         }
 
+        //Bu seriyi eleman sayısına bölerek harmonik serinin ortalamasını buluyoruz.
         double harmonicAverage = list.length/harmonic;
         System.out.println(harmonicAverage);
     }
